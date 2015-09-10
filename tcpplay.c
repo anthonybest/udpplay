ee7eeaeab05a7f62e6a945e4567e380d093dbe96
Anthony Best <anthonybest@bestanthony.com>
2015-09-10 14:48:25 -0600

    Cleanup indent

--- tcpplay.c
+++ tcpplay.c
@@ -1,3 +1,4 @@
+
 /*
 abest: cheap tcp
 */
@@ -23,61 +24,59 @@ int main(int argc, char *argv[])
 	struct sockaddr_in sAddr;
 	//struct sockaddr_in cAddr;
 	struct hostent *he;
-	char * buf;
+	char *buf;
 	int buflen = 0;
-	char * p;
+	char *p;
 
 	//ARGS
-	if(argc<2)
+	if (argc < 2)
 	{
 		fprintf(stderr, "%s <host> <port>\n", argv[0]);
 		exit(1);
 	}
 	port = atoi(argv[2]);
 
-	he = gethostbyname (argv[1]);
-	if(he == NULL)
+	he = gethostbyname(argv[1]);
+	if (he == NULL)
 	{
-		fprintf(stderr, "%s: Unable to resolve host '%s' \n", argv[0],
-				argv[1]);
+		fprintf(stderr, "%s: Unable to resolve host '%s' \n", argv[0], argv[1]);
 		exit(1);
 	}
 
 	sAddr.sin_family = he->h_addrtype;
-	memcpy((char*) &sAddr.sin_addr.s_addr, he->h_addr_list[0], he->h_length);
+	memcpy((char *) &sAddr.sin_addr.s_addr, he->h_addr_list[0], he->h_length);
 	sAddr.sin_port = htons(port);
 
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if(0 > sock)
+	if (0 > sock)
 	{
 		fprintf(stderr, "%s: unable to open socket\n", argv[0]);
 		exit(1);
 	}
 
 	/*
-    cAddr.sin_family = AF_INET;
+	cAddr.sin_family = AF_INET;
 	cAddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	cAddr.sin_port = htons(0);
 
 	if( 0 > bind(sock, (struct sockaddr *) &cAddr, sizeof(cAddr)))
 	{
-		fprintf(stderr, "%s: unable to bind socket\n", argv[0]);
-		exit(1);
+	fprintf(stderr, "%s: unable to bind socket\n", argv[0]);
+	exit(1);
 	}
 	*/
-	if(-1 == connect(sock, (struct sockaddr *)&sAddr, sizeof(struct sockaddr)))
+	if (-1 == connect(sock, (struct sockaddr *) &sAddr, sizeof(struct sockaddr)))
 	{
 		fprintf(stderr, "%s: unable to connect\n", argv[0]);
 		exit(1);
 	}
 
-
 	// Not exactly the best method.
 	len = 0;
-	p = buf = malloc(sizeof(char)*BUFSIZE);
+	p = buf = malloc(sizeof(char) * BUFSIZE);
 	buflen = BUFSIZE;
 
-	if(!buf)
+	if (!buf)
 	{
 		fprintf(stderr, "%s: malloc failed\n", argv[0]);
 		exit(1);
@@ -85,11 +84,10 @@ int main(int argc, char *argv[])
 
 	do
 	{
-		if(len == buflen)
+		if (len == buflen)
 		{
-			buf = (char * )realloc(buf,
-					(sizeof(char) * buflen) + (sizeof(char) * BUFSIZE));
-			if(!buf)
+			buf = (char *) realloc(buf, (sizeof(char) * buflen) + (sizeof(char) * BUFSIZE));
+			if (!buf)
 			{
 				fprintf(stderr, "%s: Realloc failed\n", argv[0]);
 				exit(1);
@@ -99,23 +97,23 @@ int main(int argc, char *argv[])
 			buflen += BUFSIZE;
 		}
 
-		z = fread(p, sizeof(char), buflen-len, stdin);
-		if(z > 0)
+		z = fread(p, sizeof(char), buflen - len, stdin);
+		if (z > 0)
 		{
 			len += z;
 			p += z;
 		}
-	} while (z>0);
+	}
+	while (z > 0);
 
 	/*
-    fwrite(buf, len, sizeof(char), stdout);
+	fwrite(buf, len, sizeof(char), stdout);
 
 	return 1;
 	*/
 
-
 	r = send(sock, buf, len, 0);
-	if( 0 > r)
+	if (0 > r)
 	{
 		fprintf(stderr, "r = %i\n", r);
 		fprintf(stderr, "%s: unable to write to socket\n", argv[0]);
